41ec6ccef75f7e742230588b4a3f4b9e4522b544
Glitch (sebastian-kaczynski-giphy-project) <none>
2022-04-12 22:07:49 +0000

    🎰🟣 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-let ApiUrl = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1"
 
 $(".search-button").click(function() {
+let UserInput = $("input").val();
+let ApiUrl = `https://api.giphy.com/v1/gifs/search?q=${UserInput}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`
+console.log(UserInput)
   fetch(ApiUrl)
     .then(function(response) {
       return response.json();
   })
     .then(function(data) {
       console.log(data["data"][0])
+    
+    $(".main").append(`${data["data"][0]["images"]["original"]["url"]}`)
+    $(".main").html(`<img src ="${data["data"][0]["images"]["original"]["url"]}">`);
+    
   })
+
 });
